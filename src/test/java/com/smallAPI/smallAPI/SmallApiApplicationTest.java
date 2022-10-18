62e411e15db3725508689fef486cfc3e7b9704bb
Pierre HERMAN <pierre.herman@epitech.eu>
2022-10-18 14:40:22 +0200

    Created project + spring boot tutorial

---
+++ src/test/java/com/smallAPI/smallAPI/SmallApiApplicationTest.java
@@ -0,0 +1,13 @@
+package com.smallAPI.smallAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallApiApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
