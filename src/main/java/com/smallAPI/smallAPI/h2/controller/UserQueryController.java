31c61b9f774cc794c277a2a41b727af31c920894
Pierre HERMAN <pierre.herman@epitech.eu>
2022-11-01 15:07:18 +0100

    Refreshed propoerties

--- src/main/java/com/smallAPI/smallAPI/h2/controller/UserQueryController.java
+++ src/main/java/com/smallAPI/smallAPI/h2/controller/UserQueryController.java
@@ -3,6 +3,9 @@ package com.smallAPI.smallAPI.h2.controller;
 import com.smallAPI.smallAPI.h2.models.User;
 import com.smallAPI.smallAPI.h2.services.UserQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.endpoint.event.RefreshEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +22,25 @@ public class UserQueryController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    @Value("${allowed}")
+    public String allowed;
+
     private final UserQueryService userQueryService;
 
     public UserQueryController(UserQueryService userQueryService) {
         this.userQueryService = userQueryService;
     }
 
+    public void fireRefreshEvent() {
+        eventPublisher.publishEvent(new RefreshEvent(this, "RefreshEvent", "Refreshing scope"));
+    }
+
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "userId") Long userId) {
+        this.fireRefreshEvent();
         Boolean allowed = Boolean.parseBoolean(environment.getProperty("allowed"));
         System.out.println(allowed);
         Optional<User> userOpt = userQueryService.getUserById(userId);
